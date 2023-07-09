126152be89f63a63b80f770b7192e2e83ccf7546
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-07-10 01:27:42 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AnonPN
+ script get last phone number fake anonymsms.com and recive last sms 
---
+++ main.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+from lxml import etree
+
+def exct_phonecart():
+    phone = ''
+    try:
+        #Request URL
+        page = requests.get("https://anonymsms.com/")
+
+        #Fetch webpage
+        soup = BeautifulSoup(page.content,"html.parser")
+
+        #Scraping Data
+        phone = str(soup.find("div",{"class":"sms-card__number"}).text.replace("\n","").replace('content_copy','').replace(' ', '').strip())
+        phone = phone[1:]
+        return phone
+    except requests.exceptions.ConnectionError:
+        print('ConnectionError: Try Again')
+
+def recive_sms(phone):
+    URL = f'https://anonymsms.com/number/{phone}/'
+    page = requests.get(URL)
+
+    #Fetch webpage
+    soup = BeautifulSoup(page.content,"html.parser")
+
+    #Scraping Data
+    phone = str(soup.find("td",{"class":"table-panel__message"}).text.replace("\n","").strip())
+    phone = phone[1:]
+    return phone
+
+if __name__ == "__main__":
+    phone = exct_phonecart()
+    lastsms = recive_sms(phone=phone)
+    print(phone, lastsms)
