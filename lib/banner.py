e79fbfe8653412301bfaa1f777ad88cb19d61f8d
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-13 10:50:57 +0330

    Update banner.py

--- lib/banner.py
+++ lib/banner.py
@@ -2,7 +2,7 @@ from colorama import Fore as color
 from colorama import Style as font
 from requests import get as g
 def home():
-    v = g('https://raw.githubusercontent.com/BDadmehr0/AnonPN/main/lib/V').text
+    v = g('https://raw.githubusercontent.com/BDadmehr0/AnonPN/main/lib/version').text
     ascii_banner = color.WHITE+f'''
    ___                  ___  _  __
   / _ | ___  ___  ___  / _ \/ |/ /  |{color.GREEN} AnonPN{font.BRIGHT} {v}{font.NORMAL}{color.WHITE}
