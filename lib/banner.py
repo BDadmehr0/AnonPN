4f8f51a1150fbd46e9710765875f8fb3ed73cab1
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-14 04:04:36 +0330

    Update banner.py

--- lib/banner.py
+++ lib/banner.py
@@ -2,27 +2,6 @@ from colorama import Fore as color
 from colorama import Style as font
 from requests import get as g
 
-''''Regular Colors
-Value	Color
-\e[0;30m	Black
-\e[0;31m	Red
-\e[0;32m	Green
-\e[0;33m	Yellow
-\e[0;34m	Blue
-\e[0;35m	Purple
-\e[0;36m	Cyan
-\e[0;37m	White
-Bold
-Value	Color
-\e[1;30m	Black
-\e[1;31m	Red
-\e[1;32m	Green
-\e[1;33m	Yellow
-\e[1;34m	Blue
-\e[1;35m	Purple
-\e[1;36m	Cyan
-\e[1;37m	White
-''''
 
 def home():
     v = g('https://raw.githubusercontent.com/BDadmehr0/AnonPN/main/lib/version').text
